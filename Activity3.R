69b95ee8190afad8344dd5b32c6727734893ba6c
wackerman23 <wackerman@colgate.edu>
2022-02-23 12:06:29 -0500

    2/23 Commit Activity 3

---
+++ Activity3.R
@@ -0,0 +1,92 @@
+#Will Ackerman
+#Activity 3
+#2/21/2022
+
+#Assert Function ----------------
+##create a function. The names of the arguments for your function will be in 
+#parentheses. Everything in curly brackets will be run each time the function is run.
+assert <- function(statement,err.message){
+  #if evaluates if a statement is true or false for a single item
+  if(statement == FALSE){
+    print(err.message)
+  }
+  
+}
+
+#check how the statement works
+#evaluate a false statement
+assert(1 == 2, "error: unequal values")
+
+#evaluate a true statement
+assert(2 == 2, "error: unequal values")
+#set up assert to check if two vectors are the same length
+a <- c(1,2,3,4)
+b <- c(8,4,5)
+assert(length(a) == length(b), "error: unequal length")
+
+#Initializing Data ----------------------------
+#set the working directory
+setwd("Z:/students/wackerman/Bewkes Weather/")
+
+# Import Bewkes Data
+datW <- read.csv("Z:\\students\\wackerman\\Bewkes Weather\\bewkes_weather.csv", 
+                    na.strings=c("#N/A"), skip=3, header=FALSE)
+#preview data
+print(datW[1,])
+
+#create sensor info data
+sensorInfo <- read.csv("Z:\\students\\wackerman\\Bewkes Weather\\bewkes_weather.csv",
+                         na.strings=c("#N/A"), nrows=2)
+
+print(sensorInfo)
+
+#get column names from sensorInfo table
+# and set weather station colnames  to be the same
+colnames(datW) <-   colnames(sensorInfo)
+#preview data
+print(datW[1,])
+
+#Lubridate Package -----
+#use install.packages to install lubridate
+install.packages(c("lubridate"))
+#it is helpful to comment this line after you run this line of code on the computer
+#and the package installs. You really don't want to do this over and over again.
+
+library("lubridate")
+
+#Convert date to standardized format -----
+#date format is m/d/y
+dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
+#calculate day of year
+datW$doy <- yday(dates)
+#calculate hour in the day
+datW$hour <- hour(dates) + (minute(dates)/60)
+#calculate decimal day of year
+datW$DD <- datW$doy + (datW$hour/24)
+#quick preview of new date calcualtions
+datW[1,]
+
+#Checking for Missing Data ----
+#see how many values have missing data for each sensor observation
+#air temperature
+length(which(is.na(datW$air.temperature)))
+#wind speed
+length(which(is.na(datW$wind.speed)))
+#precipitation
+length(which(is.na(datW$precipitation)))
+#soil temperature
+length(which(is.na(datW$soil.moisture)))
+#soil moisture
+length(which(is.na(datW$soil.temp)))
+
+#Visual Checks ------
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+
