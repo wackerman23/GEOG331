4e0f7b0e1963bfab2628e63a273757b116a0b9b9
wackerman23 <wackerman@colgate.edu>
2022-02-28 13:47:11 -0500

    2/28 Activity 3 work

--- Activity3.R
+++ Activity3.R
@@ -26,16 +26,16 @@ assert(length(a) == length(b), "error: unequal length")
 
 #Initializing Data ----------------------------
 #set the working directory
-setwd("Z:/students/wackerman/Bewkes Weather/")
+setwd("/Users/willackerman/GEOG331/Bewkes Weather")
 
 # Import Bewkes Data
-datW <- read.csv("Z:\\students\\wackerman\\Bewkes Weather\\bewkes_weather.csv", 
+datW <- read.csv("/Users/willackerman/GEOG331/Bewkes Weather/bewkes_weather.csv", 
                     na.strings=c("#N/A"), skip=3, header=FALSE)
 #preview data
 print(datW[1,])
 
 #create sensor info data
-sensorInfo <- read.csv("Z:\\students\\wackerman\\Bewkes Weather\\bewkes_weather.csv",
+sensorInfo <- read.csv("/Users/willackerman/GEOG331/Bewkes Weather/bewkes_weather.csv",
                          na.strings=c("#N/A"), nrows=2)
 
 print(sensorInfo)
@@ -89,4 +89,62 @@ plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
 #line lines
 plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
      ylab="Air temperature (degrees C)")
+
+#I'm going to make a new column to work with that indicates that I am conducting QAQC
+#because overwriting values should be done cautiously and can lead to confusing issues.
+#It can be particularly confusing when you are just learning R.
+#Here I'm using the ifelse function
+#the first argument is a logical statement to be evaluated as true or false on a vector
+#the second argument is the value that my air.tempQ1 column will be given if the statement
+#is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
+#In this case it is just given the air temperature value
+datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+
+#Realistic Values -----
+
+#check the values at the extreme range of the data
+#and throughout the percentiles
+quantile(datW$air.tempQ1)
+
+#look at days with really low air temperature
+datW[datW$air.tempQ1 < 8,]  
+
+#look at days with really high air temperature
+datW[datW$air.tempQ1 > 33,] 
+
+#measurements outside of sensor capability -----
+#plot precipitation and lightning strikes on the same plot
+#normalize lighting strikes to match precipitation
+lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
+#make the plot with precipitation and lightning activity marked
+#make it empty to start and add in features
+plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
+     type="n")
+#plot precipitation points only when there is precipitation 
+#make the points semi-transparent
+points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
+       col= rgb(95/255,158/255,160/255,.5), pch=15)        
+
+#plot lightning points only when there is lightning     
+points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
+       col= "tomato3", pch=19)
+
+#Question 5
+assert(length(lightscale > 0) == length(datW$lightning.acvitivy > 0), "error: unequal lengths")
+
+#filter out storms in wind and air temperature measurements
+# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
+#create a new air temp column
+datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                          ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
+
+
+#Question 6
+#filter out storms in wind and air temperature measurements
+# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm. 
+# filter windspeeds below .5mph and gust speeds over 4mph
+#create a new air temp column
+datW$air.tempQ3 <- ifelse(datW$wind.speed  >= .5 & datW$gust.speed > 4, NA, datW$air.tempQ2)
+
+length(datW$air.tempQ3[!is.na(datW$air.tempQ3)])
 
