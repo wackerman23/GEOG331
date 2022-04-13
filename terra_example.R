475675bbe52981827100ad7a2db06d2639ae1d39
wackerman23 <wackerman@colgate.edu>
2022-04-13 12:01:36 -0400

    4/13 terra commit

--- terra_example.R
+++ terra_example.R
@@ -1,5 +1,5 @@
 #load the terra package
-install.packages("terra")
+install.packages("tidyverse")
 library(terra)
 
 #set working directory
@@ -17,4 +17,24 @@ plotRGB(p, r = 3, g = 2, b = 1)
 plotRGB(p, r = 3, g = 2, b = 1,
         scale = 65535,
         stretch = "hist")
+
+#read file with field observations of canopy cover
+tree <- read.csv("Z:\\data\\rs_data\\siberia_stand_data.csv",
+                  header = T)
+
+
+#convert t vector object using terra package
+gtree <-vect(tree, geom = c("Long", "Lat"), "epsg:4326")
+
+#project the data to match the coordinate system of the raster layer
+gtree2 <- project(gtree, p)
+
+#create a polygon from the extent of the points
+b <- as.lines(ext(gtree), "epsg:4326")
+
+#reproject the polygons to the same projection as our raster
+b2 <- project(b, crs(p))
+
+#buffer the extent by 200m
+b3 <- buffer(b2, width = 200)
 
